09aeca9dbaede927b0232f9ea7e3d081b7ea5436
SimoCrash <simone.crescenzi@hotmail.com>
2022-11-15 20:13:53 +0100

    milestone1:senza testosbarrato codice provato l-44

---
+++ js/script.js
@@ -0,0 +1,37 @@
+new Vue ({
+    el: "#root",
+    data: {
+        todos: [
+            {
+                text: "comprare stendino",
+                done: true,
+            },
+            {
+                text: "lavare i piatti",
+                done: true,
+            },
+            {
+                text: "lavare i panni",
+                done: true,
+            },
+        ]
+    },
+    methods: {
+        deleteTodo(index){
+            this.todos.splice(index, 1);
+            // let arrExecuted = [];
+            // let arrExecuteder = this.todos.splice(index, 1);
+            // arrExecuted.push(arrExecuteder[this.todos])
+            // console.log(arrExecuted);
+            // if(arrExecuted.length != 0) {
+            //     let del = document.querySelector(".delete")
+            //     let ulList = document.createElement("ul");
+            //     let liList = document.createElement("li");
+            //     liList = arrExecuted;
+            //     ulList.append(liList);
+            //     del.append(ulList);
+            // }
+        }
+    }
+}) 
+
